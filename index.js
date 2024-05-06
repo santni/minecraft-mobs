b4e01108e6c92ff7b0c0a5b15f1a0a1db758393a
Nicolly <123502051+santni@users.noreply.github.com>
2024-05-06 15:54:15 -0300

    rota get

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Pool } = require('pg');
 
 const app = express();
-const PORT= 7007;
+const port= 7007;
 
 const pool = new Pool({
     user: 'postgres',
@@ -30,3 +30,7 @@ app.get('/mobs', async (req, res) => {
        res.status(500).send('Erro ao obter todos os mobs');
     }
 });
+
+app.listen(port, () => {
+    console.log(`Servidor rodando corretamente na porta ${port}`);
+  });
