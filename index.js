85a3dc69e6372c1068f005b8b8fa2571ee68d78d
Nicolly <123502051+santni@users.noreply.github.com>
2024-05-06 15:37:56 -0300

    rota get - test

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+const express = require('express');
+const { Pool } = require('pg');
 
+const app = express();
+const PORT= 7007;
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'minecraft',
+    password: 'ds564',
+    port: 7007,
+});
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('A rota está funcionado!')
+});
+
+app.get('/mobs', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM mobs');
+        res.json({
+            total: result.rowCount,
+            heroes: result.rows,
+        });
+    } catch (error) {
+       console.error('Erro ao obter todos os mobs', error); 
+       res.status(500).send('Erro ao obter todos os mobs');
+    }
+});
